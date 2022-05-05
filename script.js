352e9ea6f7b4a5f4950a644a54b0561c3dcacd51
Igor - Ubuntu <igorweiss@hotmail.com.br>
2022-05-05 16:36:14 -0300

    Mobile Menu Unfinished

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let nav = document.querySelectorAll('nav')[0]
+let menu = document.querySelector('.menu')
+let menuBtn = document.querySelector('#menuBtn')
 
-onscroll = ()=> {
+onscroll = checkScroll
+
+function checkScroll() {
   if (scrollY > 0) {
     nav.classList.add('scroll')
   } else {
     nav.classList.remove('scroll')
+  }
+}
+
+function hideMenu() {
+  menu.classList.add('menuHide')
+  checkScroll()
+}
+
+function showMenu() {
+  if(menu.classList.contains('menuHide')) {
+    menu.classList.remove('menuHide')
+    nav.classList.add('scroll')
+  } else {
+    hideMenu()
   }
 }
